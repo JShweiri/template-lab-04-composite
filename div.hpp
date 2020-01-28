22fc85657ecd9890364bc7d205fb56d3a90f5070
JShweiri <wafflemanz3000@gmail.com>
2020-01-28 08:57:47 -0800

    implemented sub op and div

---
+++ div.hpp
@@ -0,0 +1,13 @@
+#ifndef __DIV_HPP__
+#define __DIV_HPP__
+
+#include "op.hpp"
+
+class Div : public Op {
+    public:
+        Div(Base *l, Base *r) : Base() {left = l; right = r;}
+        virtual double evaluate() { return left->evaluate / right->evaluate(); }
+        virtual std::string stringify() { return left->stringify() + " / " + right->stringify(); }
+};
+
+#endif //__OP_HPP__
---
+++ sub.hpp
@@ -0,0 +1,13 @@
+#ifndef __SUB_HPP__
+#define __SUB_HPP__
+
+#include "op.hpp"
+
+class Sub : public Op {
+    public:
+        Sub(Base *l, Base *r) : Base() {left = l; right = r;}
+        virtual double evaluate() { return left->evaluate - right->evaluate(); }
+        virtual std::string stringify() { return left->stringify() + " - " + right->stringify(); }
+};
+
+#endif //__OP_HPP__
